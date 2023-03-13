ad5f618acd934790252f0b6a2c3df6e19a0ae84c
jacinta mwangi <jacinta.mwangi@student@moringaschool.com>
2023-03-13 21:10:20 +0300

    Done

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,45 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  tags = ["agent", 2, "data", 7]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = [1, 2, 3] 
+  element = "arrays!"
+  array << element
+
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = ["an", "array"]
+  element = "wow"
+  array.unshift element
 end
 
 def remove_element_from_end_of_array(array)
-  
+    array = ["an", "arrays!"]
+    array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+    array = ["wow", "I", "am", "array"]
+    array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+    array = ["wow", "I", "am", "arrays!"]
+    array[2]
 end
 
 def retrieve_first_element_from_array(array)
-  
+    array = ["wow", "I", "am", "arrays!"]
+    return array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  
+    array = ["wow", "I", "am", "arrays!"]
+    return array[-1]
 end
